dfec74d709c47fd911ea677ba053c764c28048d4
Safeya-Yasien <ssafeya5@gmail.com>
2025-08-18 19:38:17 +0300

    feat: update Categories component to highlight selected category in UI

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -9,6 +9,8 @@ const Categories = () => {
   const pathname = usePathname();
   const { push } = useRouter();
 
+  const selectedCategory = searchParams.get("category") || "all";
+
   const handleClick = (slug: string) => {
     const params = new URLSearchParams(searchParams);
 
@@ -20,7 +22,9 @@ const Categories = () => {
     <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
       {categories.map((category) => (
         <button
-          className="flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md"
+          className={`flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${
+            selectedCategory === category.slug ? "bg-white" : "text-gray-500"
+          }`}
           aria-label={category.name}
           key={category.slug}
           onClick={() => handleClick(category.slug)}
